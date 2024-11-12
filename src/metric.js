3459a5410e9e44b0ca330a7b7d9e59a0c7112874
ethankaloi <ekaloi123@gmail.com>
2024-11-12 16:21:12 -0700

    Added functionality for new metrics required for development

--- src/metric.js
+++ src/metric.js
@@ -1,4 +1,5 @@
 const config = require('./config.json');
+const os = require('os');
 
 class Metrics {
   constructor() {
@@ -6,7 +7,14 @@ class Metrics {
     this.postRequests = 0;
     this.getRequests = 0;
     this.delRequests = 0;
-    this.delRequests = 0;
+    this.putRequests = 0;
+    this.activeUsers =  0;
+    this.successfulLogins = 0;
+    this.failedLogins = 0;
+    this.pizzaSoldPerMinute = 0;
+    this.creationFailures = 0;
+    this.revenuePerMinute = 0;
+    this.latency = [];
 
     // This will periodically sent metrics to Grafana
     const timer = setInterval(() => {
@@ -15,8 +23,41 @@ class Metrics {
       this.sendMetricToGrafana('request', 'delete', 'total', this.delRequests);
       this.sendMetricToGrafana('request', 'get', 'total', this.getRequests);
       this.sendMetricToGrafana('request', 'put', 'total', this.putRequests);
+      this.sendMetricNonReqToGrafana('CPU Usage', 'CPU', getCpuUsagePercentage());
+      this.sendMetricNonReqToGrafana('Memory Usage', 'Memory', getMemoryUsagePercentage());
+      this.sendMetricNonReqToGrafana('Active users', 'users', this.activeUsers);
+      this.sendMetricNonReqToGrafana('Successful Login', 'Login', this.successfulLogins);
+      this.sendMetricNonReqToGrafana('Failed Login Attempts', 'Login Failed', this.failedLogins);
+      this.sendMetricNonReqToGrafana('Pizzas Sold', 'Pizza', this.pizzaSoldPerMinute);
+      this.sendMetricNonReqToGrafana('Creation Failures','creation failed', this.creationFailures);
+      this.sendMetricNonReqToGrafana('Revenue per minute', 'Rev', this.revenuePerMinute);
+      this.sendMetricNonReqToGrafana('Latency', 'Lat', this.latency);
     }, 10000);
     timer.unref();
+  }
+
+  incrementPizzaSold(){
+    this.pizzaSoldPerMinute++;
+  }
+
+  addRevenue(profit){
+    this.revenuePerMinute += profit;
+  }
+
+  incrementSuccessfulLogin(){
+    this.successfulLogins++;
+  }
+
+  incrementFailedLogins(){
+    this.failedLogins++;
+  }
+
+  incrementActiveUsers(){
+    this.activeUsers++;
+  }
+
+  decrementActiveUSers(){
+    this.activeUsers--;
   }
 
   incrementRequests() {
@@ -58,6 +99,41 @@ class Metrics {
         console.error('Error pushing metrics:', error);
       });
   }
+
+  sendMetricNonReqToGrafana(metricPrefix, metricName, metricValue) {
+    const metric = `${metricPrefix},source=${config.source} ${metricName}=${metricValue}`;
+
+    fetch(`${config.url}`, {
+      method: 'post',
+      body: metric,
+      headers: { Authorization: `Bearer ${config.userId}:${config.apiKey}` },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error('Failed to push metrics data to Grafana');
+        } else {
+          console.log(`Pushed ${metric}`);
+        }
+      })
+      .catch((error) => {
+        console.error('Error pushing metrics:', error);
+      });
+  }
+
+
+
+    getCpuUsagePercentage() {
+        const cpuUsage = os.loadavg()[0] / os.cpus().length;
+        return cpuUsage.toFixed(2) * 100;
+    }
+
+     getMemoryUsagePercentage() {
+        const totalMemory = os.totalmem();
+        const freeMemory = os.freemem();
+        const usedMemory = totalMemory - freeMemory;
+        const memoryUsage = (usedMemory / totalMemory) * 100;
+        return memoryUsage.toFixed(2);
+    }   
 }
 
 const metrics = new Metrics();
